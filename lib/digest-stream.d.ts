e928b1a4b7d880ed275d7ecd7b8390507e4262f0
Connor White <connor.bcw@gmail.com>
2020-04-19 21:37:50 -0500

    Update typings

--- lib/digest-stream.d.ts
+++ lib/digest-stream.d.ts
@@ -1,11 +1,23 @@
-declare module "digest-stream" {
-  import { HexBase64BinaryEncoding, Utf8AsciiBinaryEncoding } from "crypto";
-  import { Transform, TransformCallback, TransformOptions } from "stream";
-  export default function digestStream(
-    algorithm: string,
-    inputEncoding: Utf8AsciiBinaryEncoding | null,
-    digestEncoding: HexBase64BinaryEncoding | null,
-    options: TransformOptions | null,
-    listenerFn: (digest: string, length: number) => Error | void
-  ): Transform;
-}
+import { HexBase64BinaryEncoding, Utf8AsciiBinaryEncoding } from "crypto";
+import { Transform, TransformOptions } from "stream";
+
+export default function digestStream(
+  algorithm: string,
+  inputEncoding: Utf8AsciiBinaryEncoding | null,
+  digestEncoding: HexBase64BinaryEncoding | null,
+  options: TransformOptions | null,
+  listenerFn: (digest: string, length: number) => Error | void
+): Transform;
+
+export default function digestStream(
+  algorithm: string,
+  digestEncoding: HexBase64BinaryEncoding | null,
+  options: TransformOptions | null,
+  listenerFn: (digest: string, length: number) => Error | void
+): Transform;
+
+export default function digestStream(
+  algorithm: string,
+  digestEncoding: HexBase64BinaryEncoding | null,
+  listenerFn: (digest: string, length: number) => Error | void
+): Transform;
